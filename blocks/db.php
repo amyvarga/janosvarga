0edbe60da8363a3625f6c84dbf1851e593e0a7af
Amy Varga <amyvarga@users.noreply.github.com>
2020-06-20 14:43:33 +0100

    Update db.php

--- blocks/db.php
+++ blocks/db.php
@@ -1,4 +1,4 @@
 <?php 
-	$db = mysqli_connect ("localhost","root","") or die("Unable to connect to mysql server: ".mysql_error());
-	mysqli_select_db($db, "happysla_happys") or die("Unable to select mysql database: ".mysql_error());
+	$db = mysql_connect ("localhost","amyvarga","0range") or die("Unable to connect to mysql server: ".mysql_error());
+	mysql_select_db("janosvarga",$db) or die("Unable to select mysql database: ".mysql_error());
 ?>
